e13b3b24a4eeae54ac8ac0695f7dda412b6f2ce7
Fatima <fatima.machhouri1@gmail.com>
2019-10-08 20:14:02 +0200

    add condition on stage files

--- src/main/scala/command/Add.scala
+++ src/main/scala/command/Add.scala
@@ -22,7 +22,7 @@ object Add {
       val cryptedContent = encryptThisString(fileContent)
 
       //Step 2 : We create the blob file with a crypted name based on the content
-      val blobFile = new File(pathBlobs + File.separator + encryptThisString(cryptedContent))
+      val blobFile = new File(pathBlobs + File.separator + cryptedContent)
       blobFile.createNewFile()
 
       //Step 3 : We write the file content in the blob
@@ -30,12 +30,38 @@ object Add {
       bw.write(fileContent)
       bw.close()
 
-      //Step 4 : We write in the stage file the arborescence created
-      val bws = new BufferedWriter(new FileWriter(currentRepositoryPath + File.separator + ".sgit" + File.separator + "STAGE", true))
-      bws.write(cryptedContent + " " + file)
-      bws.newLine();
+      //Step 4 : We get the stage content
+      val stage = new File(".").getCanonicalPath + File.separator + ".sgit" + File.separator + "STAGE"
+      val stageContent = Source.fromFile(stage).mkString
+
+      //Step 5 : We check if the file is already staged
+      val newStageContent = {
+        if (alreadyStaged(file)) {
+          val stageContentWithoutFile = stageContent.split("\n").filter(!_.contains(file))
+          stageContentWithoutFile.mkString("\n") + "\n" + cryptedContent + " " + file
+        }
+        else if (stageContent == "") cryptedContent + " " + file
+        else stageContent + "\n" + cryptedContent + " " + file
+      }
+
+      //Step 6 : We write the content in the stage file
+      val bws = new BufferedWriter(new FileWriter(currentRepositoryPath + File.separator + ".sgit" + File.separator + "STAGE"))
+      bws.write(newStageContent.toString)
       bws.close()
+
     })
+  }
+
+  /**
+   *
+   * @param file
+   * @return
+   * Return true if the file is already is the stage else false
+   */
+  private def alreadyStaged(file: String): Boolean = {
+    val path = new File(".").getCanonicalPath + File.separator + ".sgit" + File.separator + "STAGE"
+    val stageContent = Source.fromFile(path).mkString
+    stageContent.contains(file)
   }
 
 }
