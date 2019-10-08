d3c814ea3b9d4b807ee498ad2c7c45f773d1ed88
Fatima <fatima.machhouri1@gmail.com>
2019-10-08 14:06:30 +0200

    Finish commit func

--- src/main/scala/command/Commit.scala
+++ src/main/scala/command/Commit.scala
@@ -9,25 +9,83 @@ object Commit {
 
   /**
    *
-   * Creates the arborescence of the stage file
+   * @return a list of Tree which corresponds to the arborescence of the stafe file
    */
-  def commit(): Unit = {
+  def commit(): List[Tree] = {
     val arrayPaths = formatStageFile()
 
-    def commitTailRec(parentTree: String, treeName: String, arrayPaths: Array[Array[String]]): List[Tree] = {
-      if (arrayPaths.length==0) return Nil
+    def createTreesPathTailRec(parentTree: String, treeName: String, arrayPaths: Array[String], listTrees: List[Tree]): List[Tree] = {
+
+      if (arrayPaths.isEmpty) {
+        listTrees
+      }
       else {
-        if (isATxtFile(arrayPaths.head.head)) {
-          createTree(parentTree, treeName, Array(arrayPaths.head.head)) :: commitTailRec(parentTree, treeName, arrayPaths.tail)
-        }
-        else { //I'm a directory
-          val newArrayPaths = arrayPaths
-          createTree(parentTree, arrayPaths.head.head, Array(arrayPaths.head.tail(0))) :: commitTailRec(arrayPaths.head.head, arrayPaths.head.tail.head, newArrayPaths)
-        }
+        //Step 1 : We create the tree associated to the path
+        val currenTree = createTreePath(arrayPaths.head.split("/"))
+
+        //Step 2 : We merge the arborescence created with the arborescence already existing
+        val newListTrees = mergeListTrees(listTrees, currenTree)
+
+        //Step 3 : We run the recursive algorithm on the next path, with the new list of trees
+        return createTreesPathTailRec(parentTree, treeName, arrayPaths.tail, newListTrees)
       }
 
     }
-    val listTrees = commitTailRec("origin", "o", arrayPaths)
+
+    createTreesPathTailRec("o", "origin", arrayPaths, List())
+  }
+
+  /**
+   *
+   * @param arrayPath
+   * @return
+   * Takes in parameter a path formatted as an Array and return the trees created by this path
+   */
+  private def createTreePath(arrayPath: Array[String]): List[Tree] = {
+
+    def createTreePathTailRec(parentTree: String, treeName: String, arrayPaths: Array[String]): List[Tree] = {
+      if (isATxtFile(arrayPaths.head)) return Nil
+      else {
+        createTree(parentTree, arrayPaths.head, Array(arrayPaths.tail.head)) :: createTreePathTailRec(arrayPaths.head, arrayPaths.head, arrayPaths.tail)
+      }
+    }
+
+    createTreePathTailRec("origin", "o", arrayPath)
+  }
+
+
+  /**
+   *
+   * @param list1
+   * @param list2
+   * @return a list of Tree
+   * Takes in parameter 2 lists and merge the second list in the first
+   * Pre-condition : the list1 must not have duplicates
+   * The result corresponds to the list1 + list2 without any duplicate and with the equal trees merged
+   */
+  private def mergeListTrees(list1: List[Tree], list2: List[Tree]): List[Tree] = {
+
+    def mergeListTreesTailRec(listTreeMerged: List[Tree], listToMerge: List[Tree]): List[Tree] = {
+      if (listToMerge.isEmpty) listTreeMerged
+      else {
+        val currentTreeToAdd = listToMerge.head
+
+        //Step 1 : We look if the tree is already existing
+        val found = listTreeMerged.exists(tree => tree.treeName == currentTreeToAdd.treeName && tree.parentTree == currentTreeToAdd.parentTree)
+
+        //Step 2 : if not, we create a new tree and we run the recursive algorithm
+        if (!found) mergeListTreesTailRec(listToMerge.head :: listTreeMerged, listToMerge.tail)
+        else { //there is already a tree existing, so we merge
+          val newListTreeMerged = listTreeMerged.map(tree => {
+            if (tree.treeName == currentTreeToAdd.treeName && tree.parentTree == currentTreeToAdd.parentTree) new Tree(tree.parentTree, tree.treeName, tree.listTrees ++ currentTreeToAdd.listTrees, tree.listBlobs ++ currentTreeToAdd.listBlobs)
+            else tree
+          })
+          mergeListTreesTailRec(newListTreeMerged, listToMerge.tail)
+        }
+      }
+    }
+
+    mergeListTreesTailRec(List(), list1 ++ list2)
   }
 
 
@@ -63,7 +121,7 @@ object Commit {
    * Each array corresponds to a path and in each array we have the path which is splitted in an array
    * For exemple : a1/b1/file1 and a2/b2/file2 --> [ [a1, b1, file1], [a2, b2, file2] ]
    */
-  private def formatStageFile(): Array[Array[String]] = {
+  private def formatStageFile(): Array[String] = {
     val currentRepositoryPath = new File(".").getCanonicalPath + File.separator + "s.git" + File.separator + "STAGE"
 
     //We retrive the STAGE file content
@@ -73,10 +131,7 @@ object Commit {
     val fileContentTab = fileContent.split("\n")
 
     //We keep only the path and we delete the blod id
-    val fileContentTabWithoutBlob = fileContentTab.map(line => line.split(" ")(1))
-
-    //Each path (array) is splitted into an array of its components (files and directories)
-    fileContentTabWithoutBlob.map(path => path.split("/"))
+    fileContentTab.map(line => line.split(" ")(1))
   }
 
 }
