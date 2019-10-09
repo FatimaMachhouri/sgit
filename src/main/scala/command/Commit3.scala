1f4492e1c87e498418a974fc4113d04e437d4936
Fatima <fatima.machhouri1@gmail.com>
2019-10-09 22:52:40 +0200

    update Tree class

--- src/main/scala/command/Commit3.scala
+++ src/main/scala/command/Commit3.scala
@@ -2,6 +2,8 @@ package command
 
 import java.io.File
 
+import entities.Tree
+
 import scala.io.Source
 
 object Commit3 {
@@ -33,6 +35,7 @@ object Commit3 {
         val create = createTrees(deepestTreesMerged)
 
         //Step 4 :
+        //TODO
         List()
       }
     }
@@ -40,20 +43,64 @@ object Commit3 {
     commitTailRec(currentStage)
   }
 
-  private def deepestTrees(listPath: List[String]): List[String] = {
+
+  private def deepestTrees(listPaths: List[String]): List[String] = {
+    //We get only the path
+    val tmpListPaths = listPaths.map(path => path.split(" ")(2))
+
+    //We retrieve the length of the deepest tree(s)
+    def deepestTreesLengthTailRec(listPaths: List[String], maxLength: Int): Int = {
+      if (listPaths.isEmpty) maxLength
+      else {
+        val currentPath = listPaths.head.split("/")
+        if (currentPath.length > maxLength) deepestTreesLengthTailRec(listPaths.tail, currentPath.length)
+        else deepestTreesLengthTailRec(listPaths.tail, maxLength)
+      }
+    }
+
+    val maxLength = deepestTreesLengthTailRec(tmpListPaths, 0)
+    listPaths.filter(path => {path.split(" ")(2).split("/").length == maxLength})
+  }
+
+
+  private def merge(listPaths: List[String]): List[Tree] = {
+
+    def mergeTailRec(listPaths: List[String], acc: List[Tree]): List[Tree] = {
+      if (listPaths.isEmpty) acc
+      else {
+        val currentPath = listPaths.head
+        val currentPathTab = currentPath.split(" ")(2).split("/")
+        val key = currentPathTab.dropRight(1).mkString("/")
+        val value = currentPath.split(" ")(0) + " " + currentPath.split(" ")(1) + " " + currentPathTab(currentPathTab.length-1)
+
+        val getSameCurrentPath = acc.filter(elem => elem.treePath == key)
+
+        if (getSameCurrentPath.isEmpty) mergeTailRec(listPaths.tail, Tree(key, List(value)) :: acc)
+        else {
+          val oldValue = acc.filter(elem => elem.treePath == key).head.content
+          val newValue = value :: oldValue
+          val newAcc = acc.filter(elem => elem.treePath != key)
+
+
+          mergeTailRec(listPaths.tail, Tree(key, newValue) :: newAcc)
+        }
+
+      }
+    }
+
+    mergeTailRec(listPaths, List())
+  }
+
+
+  private def createTrees(listPaths: List[Tree]): List[String] = {
     List()
   }
 
-  private def merge(listPath: List[String]): List[Map[String, String]] = {
-    List()
-  }
 
-  private def createTrees(listPath: List[Map[String, String]]): List[String] = {
-    List()
-  }
-
-  private def areAllOriginChildren(listPath: List[String]): Boolean = {
-    true
+  private def areAllOriginChildren(listPaths: List[String]): Boolean = {
+    val tmpListPaths = listPaths.map(path => path.split(" ")(2))
+    val elementsWithSubElements = tmpListPaths.filter(path => path.split("/").length > 1)
+    elementsWithSubElements.isEmpty
   }
 
 }
--- src/main/scala/entities/Tree.scala
+++ src/main/scala/entities/Tree.scala
@@ -3,28 +3,14 @@ package entities
 import utils.Hash.encryptThisString
 
 case class Tree(
-                 parentTree: String,
-                 treeName: String,
-                 listTrees: Array[String],
-                 listBlobs: Array[String]
-               ) {
-
+                 treePath: String,
+                 content: List[String]
+               ){
   /**
    *
    * @return a String corresponding to the hash based on the content of the tree
    */
   def idTree(): String = {
-    val stringTrees = encryptThisString(listTrees.mkString(""))
-    val stringBlobs = encryptThisString(listBlobs.mkString(""))
-    encryptThisString(treeName+stringTrees+stringBlobs)
+    encryptThisString(content.mkString("/n"))
   }
-
-  /**
-   *
-   * @return a boolean : true if the tree contains a subtree otherwise false
-   */
-  def containASubtTree(): Boolean = {
-    listTrees.size != 0
-  }
-
 }
