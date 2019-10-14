0be0dbe139c1349107672ffce7f0f4157305cecc
Fatima <fatima.machhouri1@gmail.com>
2019-10-14 02:23:39 +0200

    Update Init tests

--- src/test/scala/InitTest.scala
+++ src/test/scala/InitTest.scala
@@ -2,33 +2,33 @@ import org.scalatest.{BeforeAndAfter, FlatSpec}
 import command.Init
 import scala.reflect.io.Directory
 import java.io.File
+import utils.FileIO.createDirectory
 
 class InitTest extends FlatSpec with BeforeAndAfter {
 
   before {
-    val sgitPath = new File(".").getCanonicalPath + File.separator + ".sgit"
-    val directory = new Directory(new File(sgitPath))
-    directory.deleteRecursively()
+    val initDirectoryTest = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
+    createDirectory(initDirectoryTest)
   }
-
 
   after {
-    val sgitPath = new File(".").getCanonicalPath + File.separator + ".sgit"
-    val directory = new Directory(new File(sgitPath))
+    val initDirectoryTest = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
+    val directory = new Directory(new File(initDirectoryTest))
     directory.deleteRecursively()
   }
-
 
   "Init" should "create a .sgit in the path" in {
     val init = Init
-    assert(init.init)
+    val initDirectoryTest = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
+    assert(init.init(initDirectoryTest))
   }
-
 
   it should "not create a .sgit if already exists in the path" in {
     val init = Init
-    init.init
-    assert(!init.init)
+    val initDirectoryTest = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
+    init.init(initDirectoryTest)
+    assert(!init.init(initDirectoryTest))
   }
+
 
 }
