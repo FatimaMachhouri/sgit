57d3583254071e29c72e6d9c7974e27a9ee8ccda
Fatima <fatima.machhouri1@gmail.com>
2019-10-14 02:23:12 +0200

    add parameter path

--- src/main/scala/command/Init.scala
+++ src/main/scala/command/Init.scala
@@ -14,7 +14,7 @@ object Init {
    * If the directory is already a SGit Repository (contains a .sgit directory), returns false otherwise creates HEAD and STAGE files and Blobs, Branches, Commits, Logs, Tags and Trees directories in .sgit directory in the path parameter directory.
    */
   def init(rootPath: String): Boolean = {
-    if (isAlreadySgitRepository()) false
+    if (isAlreadySgitRepository(rootPath)) false
 
     else {
       val files: List[String] = List("HEAD", "STAGE", "STAGECOMMIT")
@@ -39,10 +39,9 @@ object Init {
    * @return a boolean
    * Returns true if the path directory contains a .sgit ie is a SGit Repository otherwise false.
    */
-  private def isAlreadySgitRepository(): Boolean = {
+  private def isAlreadySgitRepository(rootPath: String): Boolean = {
     //We get the current repository
-    val currentPath = new File(".").getCanonicalPath
-    val currentDirectory = new File(currentPath + File.separator + ".sgit")
+    val currentDirectory = new File(rootPath + File.separator + ".sgit")
     currentDirectory.exists()
   }
 
