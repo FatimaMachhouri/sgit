a715f9b5f5c3ccbf8d135a811f771dc20ab15bf8
Fatima <fatima.machhouri1@gmail.com>
2019-10-14 02:07:44 +0200

    add path parameter to Init

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -19,7 +19,12 @@ object Parser extends App {
         .text("Here : How to use sgit"),
       cmd("init")
         .action((_, c) => c.copy(mode = "init"))
-        .text("Creates a .sgit directory in the current directory."),
+        .text("Creates a .sgit directory in the current directory.")
+        .children(
+          arg[String]("<path>")
+            .optional()
+            .action((x, c) => c.copy(path = x))
+        ),
       cmd("add")
         .action((_, c) => c.copy(mode = "add"))
         .text("Stages the files.")
@@ -47,7 +52,8 @@ object Parser extends App {
 
       config.mode match {
         case "init" => {
-          Init.init()
+          val currentPath = new File(".").getCanonicalPath
+          if (args.length == 1) Init.init("") else Init.init(currentPath + File.separator + args(1))
         }
 
         case "add" => {
