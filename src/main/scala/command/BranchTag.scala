18e3a17b71d6dc63e3b7d42c6507dddec1b080e8
Fatima <fatima.machhouri1@gmail.com>
2019-10-17 21:20:42 +0200

    add initial commit condition

--- src/main/scala/command/BranchTag.scala
+++ src/main/scala/command/BranchTag.scala
@@ -10,12 +10,19 @@ object BranchTag {
    *
    * @param rootPath
    * @param branchName
+   * @return Boolean
+   * //We can't create a branch if number of commits == 0
    */
-  def createBranch(rootPath: String, branchName: String): Unit = {
+  def createBranch(rootPath: String, branchName: String): Boolean = {
     val currentBranch = getContentFile(rootPath + File.separator + ".sgit" + File.separator + "HEAD")
-    val lastCommit = getContentFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + currentBranch)
-    createFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + branchName)
-    writeInFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + branchName, lastCommit)
+    val nbCommits = getFilesDirectory(rootPath + File.separator + ".sgit" + File.separator + "Commits")
+    if (nbCommits.nonEmpty) {
+      val lastCommit = getContentFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + currentBranch)
+      createFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + branchName)
+      writeInFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + branchName, lastCommit)
+      true
+    }
+    else false
   }
 
 
@@ -23,12 +30,19 @@ object BranchTag {
    *
    * @param rootPath
    * @param tag
+   * @return Boolean
+   * //We can't create a tag if number of commits == 0
    */
-  def createTag(rootPath: String, tag: String): Unit = {
+  def createTag(rootPath: String, tag: String): Boolean = {
     val currentBranch = getContentFile(rootPath + File.separator + ".sgit" + File.separator + "HEAD")
-    val lastCommit = getContentFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + currentBranch)
-    createFile(rootPath + File.separator + ".sgit" + File.separator + "Tags" + File.separator + tag)
-    writeInFile(rootPath + File.separator + ".sgit" + File.separator + "Tags" + File.separator + tag, lastCommit)
+    val nbCommits = getFilesDirectory(rootPath + File.separator + ".sgit" + File.separator + "Commits")
+    if (nbCommits.nonEmpty) {
+      val lastCommit = getContentFile(rootPath + File.separator + ".sgit" + File.separator + "Branches" + File.separator + currentBranch)
+      createFile(rootPath + File.separator + ".sgit" + File.separator + "Tags" + File.separator + tag)
+      writeInFile(rootPath + File.separator + ".sgit" + File.separator + "Tags" + File.separator + tag, lastCommit)
+      true
+    }
+    else false
   }
 
 
