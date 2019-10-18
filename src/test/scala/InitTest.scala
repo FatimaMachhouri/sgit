996aa297a8fde6e68a0003b18e3282d0d14c1e38
Fatima <fatima.machhouri1@gmail.com>
2019-10-19 00:23:33 +0200

    Init : add tests and refacto

--- src/test/scala/InitTest.scala
+++ src/test/scala/InitTest.scala
@@ -3,6 +3,8 @@ import command.Init
 import scala.reflect.io.Directory
 import java.io.File
 import utils.FileIO.createDirectory
+import utils.Path.getContentDirectory
+
 
 class InitTest extends FlatSpec with BeforeAndAfter {
 
@@ -15,19 +17,33 @@ class InitTest extends FlatSpec with BeforeAndAfter {
     val initTestDirectory = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
     val directory = new Directory(new File(initTestDirectory))
     directory.deleteRecursively()
+
+    val sgitCurrentDirectoryPath = new File(".").getCanonicalPath + File.separator + ".sgit"
+    val currentDirectory = new Directory(new File(sgitCurrentDirectoryPath))
+    currentDirectory.deleteRecursively()
   }
 
-  "Init" should "create a .sgit in the path" in {
+  "Init" should "create a .sgit" in {
     val initTestDirectory = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
-    val init = Init
-    assert(init.init(initTestDirectory))
+    assert(Init.init(initTestDirectory))
+  }
+
+  it should "create a .sgit in the path parameter" in {
+    val initTestDirectory = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
+    Init.init(initTestDirectory)
+    assert(getContentDirectory(initTestDirectory).contains(initTestDirectory + File.separator + ".sgit"))
   }
 
   it should "not create a .sgit if already exists in the path" in {
     val initTestDirectory = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
-    val init = Init
-    init.init(initTestDirectory)
-    assert(!init.init(initTestDirectory))
+    Init.init(initTestDirectory)
+    assert(!Init.init(initTestDirectory))
+  }
+
+  it should "create a .sgit in the path which contains Blobs, Branches, Commits, Logs, Tags, Trees directories and HEAD, STAGE and STAGECOMMIT files" in {
+    val initTestDirectory = new File(".").getCanonicalPath + File.separator + "InitDirectoryTest"
+    Init.init(initTestDirectory)
+    assert(getContentDirectory(initTestDirectory + File.separator + ".sgit").length == 9)
   }
 
 }
